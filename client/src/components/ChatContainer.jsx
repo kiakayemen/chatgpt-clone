0524ab2bd193c9ed54019e7165b6ee531e2a2d69
Kiarash Karimian <KiarashKarimian@outlook.com>
2023-07-04 23:21:40 +0330

    responsive adjustment but without sidebar on mobile

--- client/src/components/ChatContainer.jsx
+++ client/src/components/ChatContainer.jsx
@@ -132,21 +132,15 @@ const ChatContainer = () => {
 	return (
 		<div className="flex items-center  flex-row dark">
 			{/* sidebar */}
-			<div className="sticky basis-1/6 top-0 bottom-0 left-0 z-40 w-64 h-screen">
+			<div className="sticky max-sm:hidden basis-1/6 top-0 bottom-0 left-0 z-40 w-64 h-screen">
 				{/* <button data-drawer-target="default-sidebar" data-drawer-toggle="default-sidebar" aria-controls="default-sidebar" type="button" className="inline-flex items-center p-2 mt-2 ml-3 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
 					<span className="sr-only">Open sidebar</span>
 					<svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
 						<path clipRule="evenodd" fillRule="evenodd" d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"></path>
 					</svg>
 				</button> */}
-				<button data-drawer-target="default-sidebar" data-drawer-toggle="default-sidebar" aria-controls="default-sidebar" type="button" className="inline-flex items-center p-2 mt-2 ml-3 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
-					<span className="sr-only">Open sidebar</span>
-					<svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-						<path clipRule="evenodd" fillRule="evenodd" d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"></path>
-					</svg>
-				</button>
 
-				<aside id="default-sidebar" className="  sticky top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidebar">
+				<aside id="default-sidebar" className="max-sm:hidden sticky top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidebar">
 					<div className="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-[#1e1e24]">
 						<button className="bg-zinc-950 duration-300 p-2 mt-2 text-center text-lg text-white rounded-lg w-full font-bold tracking-wide focus:outline-none focus:ring-2 mb-8 hover:bg-gray-700 focus:ring-gray-600" onClick={createNewChat}>
 							+ New Chat
@@ -168,11 +162,17 @@ const ChatContainer = () => {
 
 			{/* chat */}
 			<div className="relative w-full max-w-full min-h-screen z-40">
-				<div className="flex text-white flex-col w-full max-w-full  pb-24 ">
+				<div className="flex text-white flex-col w-full max-w-full pb-24 ">
 					<div ref={chatContainerRef} className=""
 					>
-						<div className="w-full shadow-lg py-4 absolute text-center font-black text-3xl bg-[#1e1e24] top-0">
-							<h1 className="">{currentTitle ? currentTitle : "KiaGPT"}</h1>
+						<div className="w-full flex items-center shadow-lg py-4 absolute text-center font-black text-3xl bg-[#1e1e24] top-0">
+							<button data-drawer-target="default-sidebar" data-drawer-toggle="default-sidebar" aria-controls="default-sidebar" type="button" className="inline-flex items-center p-2 mt-2 ml-3 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
+								<span className="sr-only">Open sidebar</span>
+								<svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+									<path clipRule="evenodd" fillRule="evenodd" d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"></path>
+								</svg>
+							</button>
+							<h1 className="basis-4/5">{currentTitle ? currentTitle : "KiaGPT"}</h1>
 						</div>
 						<div className="chat py-8 px-12 my-10">
 							{currentChat?.map((chatMessage, index) =>
@@ -194,8 +194,8 @@ const ChatContainer = () => {
 					</div>
 
 				</div>
-				<form ref={formRef} onSubmit={handleSubmit} className='fixed bg-[#40414F] bottom-0  px-10 w-5/6 p-3 flex flex-row gap-3'>
-					<textarea className='w-full text-white bg-transparent text-base p-3 rounded-lg outline-none border-none max-w-full
+				<form ref={formRef} onSubmit={handleSubmit} className='fixed bg-[#40414F] bottom-0 right-0  px-10 w-full max-w-5/6 p-3 flex flex-row gap-3'>
+					<textarea className='w-full text-white bg-transparent text-base p-3 rounded-lg outline-none focus:ring-gray-600 border-none max-w-full
 				' name="prompt" value={prompt} onChange={e => setPrompt(e.target.value)} cols="1" rows="1" placeholder='Ask chatGPT...'></textarea>
 					<button className=" outline-0 border-0 cursor-pointer bg-transparent" type="submit">
 						<img className="w-[30px] h-[30px]" src="src/assets/send.svg" alt="Send" />
